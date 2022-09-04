6f6f26d0e0baeb6e98b8f2b28c8c43581c18dd67
bihara2 <wdbihara@gmail.com>
2022-09-05 01:00:49 +0530

    fixed url parameter bug

--- src/functions/employee/fetch.ts
+++ src/functions/employee/fetch.ts
@@ -3,7 +3,7 @@ import {Employee} from 'src/entities/employee.entity';
 import {fetch} from './employee-service';
 
 const fetchEmployee = async (event) => {
-  const employee: Employee = await fetch(event.path.empId);
+  const employee: Employee = await fetch(event.pathParameters.empid);
   return successResponse({employee});
 };
 
